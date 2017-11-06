a45e9f19c485d1eb4da8af57b38e7aacedf05cc8
Vladimir Mishko <vladimir.mishko@namecheap.com>
2017-11-06 16:26:04 +0200

    search form component added with basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule
