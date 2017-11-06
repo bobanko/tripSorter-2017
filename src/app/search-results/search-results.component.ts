79bb7928d3f2afef2c8c529ae34d77edc52d2eab
Vladimir Mishko <vladimir.mishko@namecheap.com>
2017-11-06 18:36:31 +0200

    basic search results markup

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Duration, ResultItem } from './result-item/result-item';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.css']
+  styleUrls: ['./search-results.component.less']
 })
 export class SearchResultsComponent implements OnInit {
 
-  constructor() { }
+  results: ResultItem[] = [];
+
+  constructor() {
+
+    this.results.push({
+      'transport': 'train',
+      'departure': 'London',
+      'arrival': 'Amsterdam',
+      'duration': new Duration({
+        'h': '3',
+        'm': '30'
+      }),
+      'cost': 160,
+      'discount': 50,
+      'reference': 'TLA0330'
+    });
+
+
+  }
 
   ngOnInit() {
   }
