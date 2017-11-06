12d49831c4fa0c4bdab4ffde1404592da2fb74ae
Vladimir Mishko <vladimir.mishko@namecheap.com>
2017-11-06 18:37:10 +0200

    result item component import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
+import { ResultItemComponent } from './search-results/result-item/result-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchFormComponent,
-    SearchResultsComponent
+    SearchResultsComponent,
+    ResultItemComponent
   ],
   imports: [
     BrowserModule
