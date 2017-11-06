2f6eda6c0d6bb1100bb4c984b9d7ee05d9caf40e
Vladimir Mishko <vladimir.mishko@namecheap.com>
2017-11-06 19:12:28 +0200

    deals get from 'server'

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Duration, ResultItem } from './result-item/result-item';
+import { DealsService } from '../services/deals.service';
+import { Deal } from '../services/deal';
 
 @Component({
   selector: 'app-search-results',
@@ -8,23 +9,11 @@ import { Duration, ResultItem } from './result-item/result-item';
 })
 export class SearchResultsComponent implements OnInit {
 
-  results: ResultItem[] = [];
+  results: Deal[] = [];
 
-  constructor() {
+  constructor(dealsService: DealsService) {
 
-    this.results.push({
-      'transport': 'train',
-      'departure': 'London',
-      'arrival': 'Amsterdam',
-      'duration': new Duration({
-        'h': '3',
-        'm': '30'
-      }),
-      'cost': 160,
-      'discount': 50,
-      'reference': 'TLA0330'
-    });
-
+    dealsService.getDeals().then(deals => this.results = deals);
 
   }
 
