53cad61a59618e560ea0f59782605d337e18b0f9
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-11-07 04:37:16 +0200

    results shown based on request url

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { DealsService } from '../services/deals.service';
-import { Deal } from '../services/deal';
+import {Component, OnInit} from '@angular/core';
+import {DealsService} from '../services/deals.service';
+import {Deal} from '../services/deal';
+import {SearchParams, SearchService} from '../services/search.service';
+import {ActivatedRoute} from '@angular/router';
+
 
 @Component({
   selector: 'app-search-results',
@@ -10,14 +13,29 @@ import { Deal } from '../services/deal';
 export class SearchResultsComponent implements OnInit {
 
   results: Deal[] = [];
+  notFound = false
 
-  constructor(dealsService: DealsService) {
+  total = {
+    cost: 0,
+    time: 0,
+  }
 
-    dealsService.getDeals().then(deals => this.results = deals);
-
+  constructor(private searchService: SearchService,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-  }
+    this.route.params.subscribe((params: SearchParams) => {
+      this.searchService.search(params).then(results => {
+        if (results.length === 0) {
+          this.notFound = true;
+          return;
+        }
+        this.results = results;
+        // this.results.push(...deals);
+        this.total.cost = results.reduce((sum, deal) => sum + deal.cost, 0);
 
+      });
+    });
+  }
 }
