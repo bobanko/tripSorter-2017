585bdcb2c0a9b8509585b8c9b23aed64ba7c9656
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-11-07 03:26:40 +0200

    basic deixtra impl

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -13,19 +13,38 @@ export class SearchParams {
 class Node {
   constructor(public name: string,
               public isOut: boolean = false,
-              public shortestPath: number = Infinity,
-              public cameFrom: Node = null) {
+              public minDistance: number = Infinity,
+              public cameBy: Edge = null) {
   }
 }
 
 class Edge {
+  public isUsed = false;
+
   constructor(public _deal: Deal,
               public weight: number,
               public from: Node,
               public to: Node) {
   }
+
+  get distance() {
+    return this.weight + this.from.minDistance;
+  }
+
 }
 
+function getNodeEdges(node: Node, allEdges: Edge[]) {
+  return allEdges.filter(edge => edge.from === node && !edge.isUsed);
+}
+
+function getMinDistanceNode(nodes: Node[]) {
+  const notOutNodes = nodes.filter(n => !n.isOut);
+
+  if (notOutNodes.length < 2) {
+    return notOutNodes.pop();
+  }
+  return notOutNodes.reduce((n1, n2) => n1.minDistance < n2.minDistance ? n1 : n2);
+}
 
 @Injectable()
 export class SearchService {
@@ -50,21 +69,40 @@ export class SearchService {
         this.nodes.find(x => x.name === deal.departure),
         this.nodes.find(x => x.name === deal.arrival));
     });
-
-    console.log(this.edges[0]);
   }
 
-  search(searchParams: SearchParams) {
-    console.log(searchParams);
-    // todo: impl
+  async search(searchParams: SearchParams) {
+    await Promise.all([
+      this.dealsService.getDeals(),
+      this.citiesService.getAllCities()
+    ]).then(([deals, cities]) => this.initGraph(deals, cities));
+
 
     const fromNode = this.nodes.find(node => node.name === searchParams.departure);
-    const toNode = this.nodes.find(node => node.name === searchParams.departure);
+    const toNode = this.nodes.find(node => node.name === searchParams.arrival);
 
-    this.edges.forEach(edge => {
+    // Deikstra
+    fromNode.minDistance = 0;
+    for (let node = fromNode;
+         node !== undefined;
+         node = getMinDistanceNode(this.nodes)) {
+      getNodeEdges(node, this.edges)
+        .forEach(edge => {
+          edge.isUsed = true;
+          if (edge.to.minDistance > edge.distance) {
+            edge.to.minDistance = edge.distance;
+            edge.to.cameBy = edge;
+          }
+        });
+      node.isOut = true;
+    }
 
+    // console.log(this.nodes);
 
-    });
+    // find path
+    for (let edge = toNode.cameBy; edge !== null; edge = edge.from.cameBy) {
+      console.log(edge._deal);
+    }
 
   }
 
