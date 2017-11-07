b466668147fa58ddec5b4a963092b67aa8095ace
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-11-07 02:17:58 +0200

    search form now calls search service with params

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,25 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {CitiesService} from '../services/cities.service';
+import {SearchParams, SearchService} from '../services/search.service';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.less']
 })
-export class SearchFormComponent implements OnInit {
+export class SearchFormComponent {
 
   departureCities: string[];
   arrivalCities: string[];
 
+  searchParams: SearchParams = new SearchParams();
 
-  constructor(private citiesService: CitiesService) {
+  constructor(private citiesService: CitiesService,
+              private searchService: SearchService) {
     citiesService.getDepartureCities()
-      .then(cities => this.departureCities = cities);
+      .then(cities => {
+        this.departureCities = cities;
+        this.searchParams.departure = cities[0];
+      });
     citiesService.getArrivalCities()
-      .then(cities => this.arrivalCities = cities);
+      .then(cities => {
+        this.arrivalCities = cities;
+        this.searchParams.arrival = cities[0];
+      });
+
+    this.searchParams.sortType = 'cheapest';
   }
 
-  ngOnInit() {
+  search(searchParams: SearchParams) {
+    this.searchService.search(searchParams);
+    // todo: show/redirect to results
   }
-
 }
